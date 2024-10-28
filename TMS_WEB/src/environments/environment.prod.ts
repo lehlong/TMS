1f1bb873ade971b4139b5dbfc08a9aea680d95f8
Hải Long <hlonn.it@gmail.com>
2024-10-28 09:29:07 +0700

    Update

--- TMS_WEB/src/environments/environment.prod.ts
+++ TMS_WEB/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
- //baseApiUrl: 'http://sso.d2s.com.vn:4443/api',
-  baseApiUrl: 'https://localhost:4008/api',
+ baseApiUrl: 'http://sso.d2s.com.vn:4443/api',
+  //baseApiUrl: 'https://localhost:4008/api',
   //baseApiUrl: 'http://localhost:5203/api',
   // thisUrl: 'https://localhost:',
   // http://sso.d2s.com.vn
